afc2d0e7ba2a4275d7b4b77ef8479f1aa7e62575
Andrey Kirichenko <rclongloaf@ya.ru>
2022-05-28 23:36:33 +0300

    Initial commit

---
+++ Assets/samples/AdvancedTutorial/scripts/Enemies/RedCube.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Photon.Bolt;
+using UnityEngine;
+using Player = Bolt.AdvancedTutorial.Player;
+
+namespace Bolt.Samples.AdvancedTutorial.scripts.Enemies
+{
+    public class RedCube : EntityBehaviour<IRedCube>
+    {
+        public override void Attached()
+        {
+            state.SetTransforms(state.transform, transform);
+        }
+
+        public override void SimulateOwner()
+        {
+            if (Player.allPlayers.Any())
+            {
+                entity.transform.position += (Player.allPlayers.First().entity.gameObject.transform.position - entity.transform.position)
+                    .normalized * 0.01f;
+            }
+        }
+        
+        public void ApplyDamage(byte damage)
+        {
+            state.health -= damage;
+
+            if (state.health > 100 || state.health < 0)
+            {
+                state.health = 0;
+            }
+
+            if (state.health == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
